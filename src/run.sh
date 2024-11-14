9b640c68ef69a8a777ccd343ea92f568c7c14c92
Lisa Schmidt <61876411+LisaRebecca@users.noreply.github.com>
2024-11-14 12:00:27 -0500

    Add experiment runs

--- src/run.sh
+++ src/run.sh
@@ -11,18 +11,27 @@ save_results=True
 optimizer=sgd
 subset_size=50000
 dry_run=False
-batch_size=64
+batch_size=4096
 epsilon=3
 delta=1e-05
-epochs=1
+epochs=10
 lr=0.001
 cpu=True
+
+#source env-dynamic/bin/activate; python main.py --model=$model --dataset=$dataset --algo=rmsprop --C=$C --C2=$C2 --save_results=$save_results --optimizer=$optimizer --subset_size=$subset_size --dry_run=$dry_run --batch_size=$batch_size --epsilon=$epsilon --delta=$delta --epochs=$epochs --lr=$lr --cpu=$cpu
+
+#source env-dynamic/bin/activate; python main.py --model=$model --dataset=$dataset --algo=adam --C=$C --C2=$C2 --save_results=$save_results --optimizer=$optimizer --subset_size=$subset_size --dry_run=$dry_run --batch_size=$batch_size --epsilon=$epsilon --delta=$delta --epochs=$epochs --lr=$lr --cpu=$cpu
+
+#source env-dynamic/bin/activate; python main.py --model=$model --dataset=$dataset --algo=adagrad --C=$C --C2=$C2 --save_results=$save_results --optimizer=$optimizer --subset_size=$subset_size --dry_run=$dry_run --batch_size=$batch_size --epsilon=$epsilon --delta=$delta --epochs=$epochs --lr=$lr --cpu=$cpu
+
+#source env-dynamic/bin/activate; python main.py --model=$model --dataset=$dataset --algo=adamw --C=$C --C2=$C2 --save_results=$save_results --optimizer=$optimizer --subset_size=$subset_size --dry_run=$dry_run --batch_size=$batch_size --epsilon=$epsilon --delta=$delta --epochs=$epochs --lr=$lr --cpu=$cpu
+
 
 if [ "$algo" = "DPSGD" ]; then
     source env-dpsgd/bin/activate; python main.py --model=$model --dataset=$dataset --algo=$algo --C=$C --C2=$C2 --save_results=$save_results --optimizer=$optimizer --subset_size=$subset_size --dry_run=$dry_run --batch_size=$batch_size --epsilon=$epsilon --delta=$delta --epochs=$epochs --lr=$lr --cpu=$cpu
 
 elif [ "$algo" = "DynamicSGD" ]; then
-    source env-dynamic/bin/activate; python main.py --algo=$algo --save_results=$save_results --optimizer=$optimizer --subset_size=$subset_size --dry_run=$dry_run --batch_size=$batch_size --epsilon=$epsilon --epochs=$epochs --lr=$lr --delta=$delta 
+    source env-dynamic/bin/activate; python main.py --model=$model --dataset=$dataset --algo=$algo --C=$C --C2=$C2 --save_results=$save_results --optimizer=$optimizer --subset_size=$subset_size --dry_run=$dry_run --batch_size=$batch_size --epsilon=$epsilon --delta=$delta --epochs=$epochs --lr=$lr --cpu=$cpu
 else
     echo "Unknown algorithm: $algo"
     exit 1
